b8c849c120c26bf3250a1af167a96e89bffe6d7a
Cesar409-dot <cesar.vequiato@sptech.school>
2025-11-16 18:17:44 -0300

    Subindo verificação de login funcional e a página perguntas, onde o usuário fará o quiz

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -15,7 +15,15 @@ function autenticar(req, res) {
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+                                    res.json({
+                                        id: resultadoAutenticar[0].idusuario,
+                                        email: resultadoAutenticar[0].email,
+                                        nome: resultadoAutenticar[0].nome,
+                                        senha: resultadoAutenticar[0].senha,
+                                });
+                    } 
                     if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
